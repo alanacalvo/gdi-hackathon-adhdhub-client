e1085d0ab44c85a726efabf5dc0c1b47e0ceae4c
alanacalvo <alana.keeton@gmail.com>
2022-11-18 13:32:53 -0500

    adding daily goal tracker

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ import HomePage from './Views/HomePage/HomePage';
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>FocusUp</h1>
-      </header>
       <Router>
         <Routes>
           <Route path='/' element={<HomePage />}></Route>
--- src/Views/HomePage/HomePage.jsx
+++ src/Views/HomePage/HomePage.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './HomePage.scss';
 import Timer from '../../Components/Timer/Timer';
 import ToDoList from '../../Components/ToDoList/ToDoList';
+import DailyGoalTracker from '../../Components/DailyGoalTracker/DailyGoalTracker';
 
 function HomePage() {
 
   return (
-  <div className="homepage">
-    <ToDoList />
-    <Timer />
-  </div>
+    <div className="homepage">
+      <ToDoList />
+      <div className="leftSide">
+        <Timer />
+        <DailyGoalTracker />
+      </div>
+    </div>
   )
 }
 
