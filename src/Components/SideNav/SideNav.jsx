6dfc03b7e48e611ddaea1b4e612eae74b35ca8c1
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-11-19 04:42:23 -0500

    Formatted the sidenav

--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import './SideNav.scss'
+import React from "react";
+import "./SideNav.scss";
 // import userProfile from './Icons/user-profile-64.png';
 // import calendar from './Icons/calendar-50.png';
 // import settings from './Icons/settings-64.png';
 // import logout from './Icons/logout-50.png';
 
 function SideNav() {
-
   return (
     <div className="sideNav">
-        <div className='navItem'>
-          P
-          {/* <img src="/user-profile-64.png" alt="" /> */}
-          {/* <img src={userProfile} alt='user profile' />; */}
+      <div>
+        <img
+          src="https://s3.amazonaws.com/shecodesio-production/uploads/files/000/054/599/original/Profile.png?1668850698"
+          alt="profile"
+          id="profile-pic"
+        />
+      </div>
+      <div>
+        <div className="navItem">
+          <i class="fa-regular fa-calendar"></i>
         </div>
-        <div>
-            <div className='navItem'>
-              C
-              {/* <img src={calendar} alt='calendar' />; */}
-            </div>
-            <div className='navItem'>
-              S
-              {/* <img src={settings} alt='settings' />; */}
-            </div>
-             <div className='navItem'>
-              L
-               {/* <img src={logout} alt='log out' />; */}
-            </div>
+        <div className="navItem">
+          <i class="fa-solid fa-clipboard"></i>
         </div>
+        <div className="navItem">
+          <i class="fa-solid fa-arrow-right-to-bracket"></i>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SideNav
+export default SideNav;
