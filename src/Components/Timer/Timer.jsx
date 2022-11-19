aefc01c3b684a5107fc0dd8fb89ef5dbef64a559
bfreitas1098 <96152402+bfreitas1098@users.noreply.github.com>
2022-11-19 01:14:03 -0500

    adjusted the styling of the timer card

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,73 +1,68 @@
-import Header from '../TimerComponents/Header/header'
-import Controls from '../TimerComponents/Controls/controls'
-import TimerDisplay from '../TimerComponents/TimerDisplay/timerdisplay'
-import Button from '../TimerComponents/Button/button'
-import Settings from '../TimerComponents/Settings/settings'
-import { useState, useEffect } from 'react';
-import useSound from 'use-sound'
-import timesUpSfx from '../../sounds/timesUp.mp3'
-import './Timer.scss'
+// import Header from '../TimerComponents/Header/header'
+import Controls from "../TimerComponents/Controls/controls";
+import TimerDisplay from "../TimerComponents/TimerDisplay/timerdisplay";
+import Button from "../TimerComponents/Button/button";
+import Settings from "../TimerComponents/Settings/settings";
+import { useState, useEffect } from "react";
+import useSound from "use-sound";
+import timesUpSfx from "../../sounds/timesUp.mp3";
+import "./Timer.scss";
 
 function Timer() {
-  const [ settingsVisible, setSettingsVisible ] = useState(false)
-  const [ timerMode, setTimerMode ] = useState('pomo')   // options: pomo, short, long
-  const [ pomoLength, setPomoLength ] = useState(25)
-  const [ shortLength, setShortLength ] = useState(3)
-  const [ longLength, setLongLength ] = useState(15)
-  const [ fontPref, setFontPref ] = useState('kumbh')         // options: kumbh, roboto, space
-  const [ accentColor, setAccentColor ] = useState('default') // options: default, blue, purple
-  const [ secondsLeft, setSecondsLeft] = useState(pomoLength * 60)
-  const [ isActive, setIsActive ] = useState(false)
-  const [ buttonText, setButtonText ] = useState('START')
+  const [settingsVisible, setSettingsVisible] = useState(false);
+  const [timerMode, setTimerMode] = useState("pomo"); // options: pomo, short, long
+  const [pomoLength, setPomoLength] = useState(25);
+  const [shortLength, setShortLength] = useState(3);
+  const [longLength, setLongLength] = useState(15);
+  const [fontPref, setFontPref] = useState("kumbh"); // options: kumbh, roboto, space
+  const [accentColor, setAccentColor] = useState("default"); // options: default, blue, purple
+  const [secondsLeft, setSecondsLeft] = useState(pomoLength * 60);
+  const [isActive, setIsActive] = useState(false);
+  const [buttonText, setButtonText] = useState("START");
 
-  const [ volume, setVolume ] = useState(1)
-  const [ timesUp ] = useSound(timesUpSfx, {
-                                volume: volume,
-                              })
+  const [volume, setVolume] = useState(1);
+  const [timesUp] = useSound(timesUpSfx, {
+    volume: volume,
+  });
 
   useEffect(() => {
-    if(isActive) {
+    if (isActive) {
       const interval = setInterval(() => {
-        setSecondsLeft(secondsLeft => secondsLeft - 1)
-      }, 1000)
-    
-      if(secondsLeft === 0) {
-        clearInterval(interval)
-        setIsActive(false)
-        setButtonText('')
-        timesUp()
+        setSecondsLeft((secondsLeft) => secondsLeft - 1);
+      }, 1000);
+
+      if (secondsLeft === 0) {
+        clearInterval(interval);
+        setIsActive(false);
+        setButtonText("");
+        timesUp();
       }
 
-      return () => clearInterval(interval)
+      return () => clearInterval(interval);
     }
-    
   }, [isActive, secondsLeft, timesUp]);
 
-
   const toggleSettingsVisibility = (event) => {
-    setSettingsVisible(!settingsVisible)
-  }
+    setSettingsVisible(!settingsVisible);
+  };
 
   const formatTimeLeft = (seconds) => {
-    return(`${Math.floor(seconds / 60)}:${
-            (seconds % 60 > 9)
-              ? seconds % 60
-              : '0' + seconds % 60
-          }`)
-  }
+    return `${Math.floor(seconds / 60)}:${
+      seconds % 60 > 9 ? seconds % 60 : "0" + (seconds % 60)
+    }`;
+  };
 
   const calcPercentage = () => {
-    if(timerMode === 'pomo') {
-      return((secondsLeft / (pomoLength * 60)) * 100)
+    if (timerMode === "pomo") {
+      return (secondsLeft / (pomoLength * 60)) * 100;
     }
-    if(timerMode === 'short') {
-      return((secondsLeft / (shortLength * 60)) * 100)
+    if (timerMode === "short") {
+      return (secondsLeft / (shortLength * 60)) * 100;
     }
-    if(timerMode === 'long') {
-      return((secondsLeft / (longLength * 60)) * 100)
+    if (timerMode === "long") {
+      return (secondsLeft / (longLength * 60)) * 100;
     }
-    
-  }
+  };
 
   return (
     <div className="pomodoro-app">
@@ -83,7 +78,7 @@ function Timer() {
         buttonText={buttonText}
         setButtonText={setButtonText}
         volume={volume}
-        />
+      />
       <TimerDisplay
         timerMode={timerMode}
         percentage={calcPercentage()}
@@ -94,26 +89,27 @@ function Timer() {
         setButtonText={setButtonText}
         volume={volume}
         setVolume={setVolume}
-        />
+      />
       <Button type="settings" toggleVisibility={toggleSettingsVisibility} />
-      <Settings visible={settingsVisible}
-                toggleSettingsVisibility={toggleSettingsVisibility} 
-                pomoLength={pomoLength}
-                setPomoLength={setPomoLength}
-                shortLength={shortLength}
-                setShortLength={setShortLength}
-                longLength={longLength}
-                setLongLength={setLongLength}
-                fontPref={fontPref}
-                setFontPref={setFontPref}
-                accentColor={accentColor}
-                setAccentColor={setAccentColor}
-                closeSettings={toggleSettingsVisibility}
-                setSecondsLeft={setSecondsLeft}
-                timerMode={timerMode}
-                />
+      <Settings
+        visible={settingsVisible}
+        toggleSettingsVisibility={toggleSettingsVisibility}
+        pomoLength={pomoLength}
+        setPomoLength={setPomoLength}
+        shortLength={shortLength}
+        setShortLength={setShortLength}
+        longLength={longLength}
+        setLongLength={setLongLength}
+        fontPref={fontPref}
+        setFontPref={setFontPref}
+        accentColor={accentColor}
+        setAccentColor={setAccentColor}
+        closeSettings={toggleSettingsVisibility}
+        setSecondsLeft={setSecondsLeft}
+        timerMode={timerMode}
+      />
     </div>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
